b87aaf5007d52aaac93e8e734aa1064dbb04ba62
Aitana <armr11@gcloud.ua.es>
2025-02-12 20:14:48 +0100

    add comentarios en HadaP1

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 namespace hada_p1
 {
     internal class HadaP1
-    {
+    {   
+        /// <summary>
+        /// Conversor de segundos a minutos
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s)
         {
             if (s <= 0) return 0;
             return s / 60;
         }
 
+        /// <summary>
+        /// Conversor de minutos a segundos
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
